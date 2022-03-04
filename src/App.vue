9f577f3ca6fa76512f5aeddba21cd9c044cdfbb5
Piotr <straszak.piotr@gmail.com>
2022-03-04 10:15:45 +0100

    Basics done

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <button @click.prevent="flag = !flag">Toggle animation</button>
+  <transition name="fade">
+    <h2 v-if="flag">Hello world!</h2>
+  </transition>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data () {
+    return {
+      flag: false
+    }
+  }
+}
+</script>
+
+<style>
+.fade-enter-from {
+  opacity: 0;
+}
+
+.fade-enter-active {
+  transition: all 0.25s linear;
+}
+
+.fade-enter-to, .fade-leave-from {
+  opacity: 100%;
+}
+
+.fade-leave-to {
+  transition: all 0.25s linear;
+  opacity: 0;
+}
+</style>
